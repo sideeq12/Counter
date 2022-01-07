6580e21d6663ba49c2812a44f3879a6ad186ccf5
sideeq12 <abdwaheed2018@gmail.com>
2022-01-07 10:11:27 +0000

    routes

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/Counter" element={
-          <div className="App">
-          <WordCount/>
-        </div>
-        } />
-      </Routes>
-    </Router>
+    <div className="App">
+    <WordCount/>
+  </div>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/Counter" element={
+         
+    //     } />
+    //   </Routes>
+    // </Router>
   );
 }
 
